7216442214909746e26798cf8c2e0ef2d18f1398
Mriganka69 <mrigankasnath@gmail.com>
2024-04-18 17:37:40 +0530

    darkmode added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var moon = document.getElementById("moon");
 
 moon.onclick = function(){
+    console.log("Moon icon clicked");
   document.body.classList.toggle("dark-theme")
   if(document.body.classList.contains("dark-theme")){
     moon.src = "sun.png";
